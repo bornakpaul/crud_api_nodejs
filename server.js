3fe6a2fb9e8c2ec7fec2f935fe5532d7c3476863
bornakpaul <bornakpaul@gmail.com>
2023-03-11 17:41:29 +0530

    Basic port setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Bornak is learning NodeJS...");
+const express = require('express');
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+     res.send(`Hello Node Api !!!`);
+});
+
+// port
+app.listen(3001, ()=>{
+     console.log(`Node Api started!`);
+});
